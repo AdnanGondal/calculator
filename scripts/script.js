d7e2480dbe88bb80a1ac499112e2ad2297854041
AdnanGondal <adnangondal1@hotmail.com>
2020-08-03 13:23:29 +0100

    multiple calculations working, clear button not quite working

--- scripts/script.js
+++ scripts/script.js
@@ -25,13 +25,12 @@ function operate(operator,num1,num2){
 
 function updateDisplay(){
     let dispStore = "";
-    let number;
+    let result;
     display = document.querySelector('#display-text');
     display.textContent = dispStore;
     let operatorUsed;
     let num1;
     let num2;
-    let operator;
 
     const buttons = document.querySelectorAll('.digits');
     const operationButs = document.querySelectorAll('.operations');
@@ -45,7 +44,15 @@ function updateDisplay(){
             dispStore += (button.textContent);
             num2 += (button.textContent);
             display.textContent = dispStore;
-    
+
+           
+
+            if (result && num1) {
+                num1 = result;
+            }
+
+            console.log("num1: " + num1);
+            console.log("num2: " + num2);
         });
     
     
@@ -56,9 +63,14 @@ function updateDisplay(){
     operationButs.forEach((operatorBut)=>{
     
         operatorBut.addEventListener('click',()=>{
-            num2 = "";
-            num1 = dispStore;
+            
+            
             operatorUsed = operatorBut.textContent;
+
+            if (num1==undefined) {num1 = dispStore;}
+            else num1 = getOperate(operatorUsed,num1,num2);
+            num2 = "";
+
             dispStore += operatorUsed;
             display.textContent = dispStore;
 
@@ -68,38 +80,39 @@ function updateDisplay(){
     
     equalBut.addEventListener('click',()=>{
         
-        //gets the result that we need
-        switch(operatorUsed){
-            case 'X':
-                result = operate(multiply,num1,num2);
-                break;
-            case '+':
-                result = operate(add,num1,num2);
-                break;
-            case '÷':
-                result = operate(divide,num1,num2);
-                break;
-            case '-':
-                result = operate(subtract,num1,num2);
-                break;
-        }
-        
-        
-        
+        //gets the result that we nee
+        result = getOperate(operatorUsed,num1,num2)
         display.textContent = result;
     });
 
 
 
     clearBut.addEventListener('click',()=>{
+        num1 = undefined;
+        num2 = undefined;
         dispStore = "";
         display.textContent = dispStore;
     });
 
-
-
-
     });
+
+    function getOperate(operatorUsed,num1,num2){
+        switch(operatorUsed){
+            case 'X':
+                return operate(multiply,num1,num2);
+                break;
+            case '+':
+                return operate(add,num1,num2);
+                break;
+            case '÷':
+                return operate(divide,num1,num2);
+                break;
+            case '-':
+                return operate(subtract,num1,num2);
+                break;
+        }
+
+    }
 
     
 
