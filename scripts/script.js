98229a17a6b0459af70297c173e8ac3021880c42
AdnanGondal <adnangondal1@hotmail.com>
2020-08-03 12:20:32 +0100

    two numbers can be operated on, clear button works, equal button added

--- scripts/script.js
+++ scripts/script.js
@@ -3,7 +3,8 @@ let display;
 updateDisplay();
 
 function add(num1,num2){
-    return num1+num2;
+    return Number(num1)+Number(num2);
+    // Number needed to avound concatenation/
 }
 
 function subtract(num1,num2){
@@ -23,14 +24,87 @@ function operate(operator,num1,num2){
 }
 
 function updateDisplay(){
-    store = ""
-    display = document.querySelector('#display-text')
-    display.textContent = store;
+    let dispStore = "";
+    let number;
+    display = document.querySelector('#display-text');
+    display.textContent = dispStore;
+    let operatorUsed;
+    let num1;
+    let num2;
+    let operator;
 
-    button0 = document.querySelector('#zero-but');
+    const buttons = document.querySelectorAll('.digits');
+    const operationButs = document.querySelectorAll('.operations');
+    const clearBut = document.querySelector('#clear-but');
+    const equalBut = document.querySelector('#equal-but');
 
-    button0.addEventListener('click',()=>{
-        store += "0"
-        display.textContent = store;
+    //makes all the buttons add to the display their own text content. 
+    buttons.forEach((button) => {
+
+        button.addEventListener('click',()=>{
+            dispStore += (button.textContent);
+            num2 += (button.textContent);
+            display.textContent = dispStore;
+    
+        });
+    
+    
+    
     });
+
+    //adds operators to the screen: 
+    operationButs.forEach((operatorBut)=>{
+    
+        operatorBut.addEventListener('click',()=>{
+            num2 = "";
+            num1 = dispStore;
+            operatorUsed = operatorBut.textContent;
+            dispStore += operatorUsed;
+            display.textContent = dispStore;
+
+        });
+    
+    //for all other important buttons:
+    
+    equalBut.addEventListener('click',()=>{
+        
+        //gets the result that we need
+        switch(operatorUsed){
+            case 'X':
+                result = operate(multiply,num1,num2);
+                break;
+            case '+':
+                result = operate(add,num1,num2);
+                break;
+            case '÷':
+                result = operate(divide,num1,num2);
+                break;
+            case '-':
+                result = operate(subtract,num1,num2);
+                break;
+        }
+        
+        
+        
+        display.textContent = result;
+    });
+
+
+
+    clearBut.addEventListener('click',()=>{
+        dispStore = "";
+        display.textContent = dispStore;
+    });
+
+
+
+
+    });
+
+    
+
+
+
+
+
 }
